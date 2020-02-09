ad20c52bd8385b9491c559557bfa55ba0887ce71
Paul Pan <panjunhong05@gmail.com>
2020-02-09 00:21:23 -0800

    Removed default message from InvalidRequestException and VehicleNotFoundException

--- src/main/java/com/mitchell/challenge/vehicle/exceptions/InvalidRequestException.java
+++ src/main/java/com/mitchell/challenge/vehicle/exceptions/InvalidRequestException.java
@@ -3,7 +3,7 @@ package com.mitchell.challenge.vehicle.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Request Body Invalid")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidRequestException extends RuntimeException {
     public InvalidRequestException(String msg) {
         super(msg);
--- src/main/java/com/mitchell/challenge/vehicle/exceptions/VehicleNotFoundException.java
+++ src/main/java/com/mitchell/challenge/vehicle/exceptions/VehicleNotFoundException.java
@@ -3,7 +3,7 @@ package com.mitchell.challenge.vehicle.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Vehicle Not Found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class VehicleNotFoundException extends RuntimeException {
     public VehicleNotFoundException(String msg) {
         super(msg);
