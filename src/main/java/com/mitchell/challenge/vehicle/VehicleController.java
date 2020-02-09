5c89d557f546767650a74f72eb734dfb78721b0a
Paul Pan <panjunhong05@gmail.com>
2020-02-09 00:03:14 -0800

    Finished basic code implementation for vehicle app

--- src/main/java/com/mitchell/challenge/vehicle/VehicleController.java
+++ src/main/java/com/mitchell/challenge/vehicle/VehicleController.java
@@ -1,4 +1,42 @@
 package com.mitchell.challenge.vehicle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("vehicles")
 public class VehicleController {
+    private final VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @GetMapping("{id}")
+    public Vehicle getVehicleById(@PathVariable Integer id) {
+        return vehicleService.getVehicleById(id);
+    }
+
+    @PostMapping
+    public void createVehicle(@RequestBody Vehicle vehicle) {
+        vehicleService.createVehicle(vehicle);
+    }
+
+    @PutMapping
+    public void updateVehicle(@RequestBody Vehicle vehicle) {
+        vehicleService.updateVehicle(vehicle);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteVehicle(@PathVariable Integer id) {
+        vehicleService.deleteVehicle(id);
+    }
 }
